19db704175598fc465f87e1d7d60dffd499452c5
minias <gnuhacker@gmail.com>
2025-04-01 16:15:15 +0800

    hdwallet

--- go.mod
+++ go.mod
@@ -4,12 +4,15 @@ go 1.23.0
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.3.4
+	github.com/btcsuite/btcutil v1.0.2
+	github.com/tyler-smith/go-bip32 v1.0.0
 	github.com/tyler-smith/go-bip39 v1.1.0
-	golang.org/x/crypto v0.36.0
 )
 
 require (
+	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
+	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec // indirect
 	github.com/btcsuite/btcd v0.20.1-beta // indirect
-	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,27 +3,25 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"fmt"
 	"log"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcutil"
 	"github.com/btcsuite/btcutil/base58"
+
+	// "github.com/ethereum/go-ethereum/accounts/abi"
+	// "github.com/ethereum/go-ethereum/common"
+	"github.com/tyler-smith/go-bip32"
 	"github.com/tyler-smith/go-bip39"
 )
 
-// TronAddressPrefix는 트론 주소의 고정 prefix (0x41)입니다.
-const TronAddressPrefix = "41"
+// TRC20 USDT 스마트 컨트랙트 주소 (Tron 메인넷 기준)
+const TRC20_USDT_CONTRACT = "0xa614f803b6fd780986a42c78ec9c7f77e6ded13c"
 
-/*
-니모닉: outside industry squirrel sponsor vocal auto general fix evidence right ranch make food burst choose manual shadow speed type also race there chaos catalog
-개인 키: fcdd865c563f23075f4c7456058485ba3e74bdbd85a0eaacf0ba8645e1979bf2
-트론 주소: TVJkmS5kR3E3MRRfT5ypnNtE3NP1wmdMAF
-
-니모닉: this latin consider lucky spoil friend senior real poem oxygen giggle fault cruise era cigar deal switch secret crater student defense horse gown purity
-개인 키: 358377b83ac3971773d4eb957516c0cd38f6849250b32c098949380ebd121854
-트론 주소: TYweJWBiozBkLDe5WnEcFQjYXkzsJEcrYe
-*/
+// Tron BIP-44 코인 ID
+const TronCoinID = 195
+const purpose = 44
+const masterPass = "12345"
 
 func main() {
 	// 1. 니모닉 생성
@@ -35,47 +33,121 @@ func main() {
 	if err != nil {
 		log.Fatalf("니모닉 생성 실패: %v", err)
 	}
-	fmt.Println("니모닉:", mnemonic)
+	log.Printf("니모닉: %s\n", mnemonic)
 
 	// 2. 니모닉을 기반으로 시드 키 생성
-	seed := bip39.NewSeed(mnemonic, "")
+	seed := bip39.NewSeed(mnemonic, masterPass)
 
-	// 3. 개인 키 생성 (secp256k1 곡선 사용)
-	privateKey, publicKey := btcec.PrivKeyFromBytes(seed[:32])
+	// 3. BIP-32 마스터 키 생성
+	masterKey, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		log.Fatalf("마스터 키 생성 실패: %v", err)
+	}
+	log.Printf("마스터 키: %s\n\n", masterKey)
+
+	CreateUserWallet(masterKey, 0, 0) // 1번째 사이트 1번째유저
+	CreateUserWallet(masterKey, 0, 1) // 1번째 사이트 2번째유저
+	CreateUserWallet(masterKey, 1, 0) // 2번째 사이트 1번째유저
+	CreateUserWallet(masterKey, 1, 1) // 2번째 사이트 2번째유저
+
+	// 7. TRC20 USDT 전송 트랜잭션 생성 (예제)
+	// toAddress := "TXXXXXXX..."    // 수신자 주소 (Base58)
+	// amount := big.NewInt(1000000) // USDT 전송량 (소수점 6자리, 1 USDT = 1,000,000)
+	// rawTx, err := createTRC20TransferTx(address, toAddress, amount)
+	// if err != nil {
+	// 	log.Fatalf("TRC20 전송 트랜잭션 생성 실패: %v", err)
+	// }
+	// log.Println("TRC20 USDT 전송 Raw Tx:", rawTx)
+}
+
+// User HD wallet create
+func CreateUserWallet(masterKey *bip32.Key, siteNo, userNo uint32) {
+	// 4. 첫 번째 주소 생성 (m/44'/195'/0'/0/n ->0)
+	accountKey, err := deriveKey(masterKey, purpose, TronCoinID, 0, siteNo, userNo)
+	if err != nil {
+		log.Fatalf("HD 지갑 키 파생 실패: %v", err)
+	}
+	log.Printf("[%d] 사이트 키: %s", siteNo, accountKey)
+
+	// 5. 개인 키 및 공개 키 생성
+	privateKey, publicKey := btcec.PrivKeyFromBytes(accountKey.Key)
 	privateKeyHex := hex.EncodeToString(privateKey.Serialize())
-	fmt.Println("개인 키:", privateKeyHex)
+	log.Printf("[%d/%d] 개인 키: %s\n", siteNo, userNo, privateKeyHex)
 
-	// 4. 공개 키에서 주소 생성
+	// 6. 트론 주소 생성
 	publicKeyBytes := publicKey.SerializeUncompressed()[1:] // 첫 바이트(0x04) 제거
-	address := GenerateTronAddress(publicKeyBytes)
-	fmt.Println("트론 주소:", address)
+	address := generateTronAddress(publicKeyBytes)
+	log.Printf("[%d/%d] 주소:%s\n\n", siteNo, userNo, address)
 }
 
-// GenerateTronAddress는 공개 키를 기반으로 트론 주소를 생성합니다.
-func GenerateTronAddress(publicKey []byte) string {
-	// 1. SHA-256 해싱
+// deriveKey는 BIP-44 HD 지갑 키를 파생합니다.
+func deriveKey(masterKey *bip32.Key, purpose, coinType, account, change, index uint32) (*bip32.Key, error) {
+	purposeKey, err := masterKey.NewChildKey(bip32.FirstHardenedChild + purpose)
+	if err != nil {
+		return nil, err
+	}
+	coinTypeKey, err := purposeKey.NewChildKey(bip32.FirstHardenedChild + coinType)
+	if err != nil {
+		return nil, err
+	}
+	accountKey, err := coinTypeKey.NewChildKey(bip32.FirstHardenedChild + account)
+	if err != nil {
+		return nil, err
+	}
+	changeKey, err := accountKey.NewChildKey(change)
+	if err != nil {
+		return nil, err
+	}
+	indexKey, err := changeKey.NewChildKey(index)
+	if err != nil {
+		return nil, err
+	}
+	return indexKey, nil
+}
+
+// generateTronAddress는 공개 키를 기반으로 트론 주소를 생성합니다.
+func generateTronAddress(publicKey []byte) string {
 	sha256Hash := sha256.Sum256(publicKey)
-
-	// 2. RIPEMD-160 해싱 (btcutil 라이브러리 사용)
 	publicKeyHash := btcutil.Hash160(sha256Hash[:])
-
-	// 3. 트론 주소 (0x41 + RIPEMD-160 해시)
 	addressBytes := append([]byte{0x41}, publicKeyHash...)
-
-	// 4. Base58Check 인코딩 (체크섬 포함)
-	return Base58CheckEncode(addressBytes)
+	return base58EncodeWithChecksum(addressBytes)
 }
 
-// Base58CheckEncode는 Base58 체크섬을 포함한 인코딩을 수행합니다.
-func Base58CheckEncode(input []byte) string {
-	// SHA-256을 두 번 적용하여 체크섬 생성
+// base58EncodeWithChecksum는 Base58 체크섬을 포함한 인코딩을 수행합니다.
+func base58EncodeWithChecksum(input []byte) string {
 	hash := sha256.Sum256(input)
 	hash = sha256.Sum256(hash[:])
-
-	// 첫 4바이트를 체크섬으로 사용
 	checksum := hash[:4]
 	fullAddress := append(input, checksum...)
-
-	// Base58로 인코딩
 	return base58.Encode(fullAddress)
 }
+
+// createTRC20TransferTx는 TRC20 USDT 전송을 위한 트랜잭션을 생성합니다.
+// func createTRC20TransferTx(from, to string, amount *big.Int) (string, error) {
+// 	// 1. TRC20 "transfer(address,uint256)" 함수 ABI 생성
+// 	transferABI, err := abi.JSON(strings.NewReader(`[{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"}]`))
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	// 2. Base58 주소를 Hex(16진수) 주소로 변환
+// 	toHex := base58ToHex(to)
+
+// 	// 3. 데이터 필드 생성 (ABI 인코딩)
+// 	data, err := transferABI.Pack("transfer", common.HexToAddress(toHex), amount)
+// 	if err != nil {
+// 		return "", err
+// 	}
+
+// 	// 4. Raw 트랜잭션 생성 (Tron 네트워크에 맞게 변형 필요)
+// 	rawTx := log.Sprintf("0x%s", hex.EncodeToString(data))
+
+// 	return rawTx, nil
+// }
+
+// base58ToHex는 Tron의 Base58 주소를 Hex 주소로 변환합니다.
+// func base58ToHex(address string) string {
+// 	addressBytes := base58.Decode(address)
+// 	hexAddress := hex.EncodeToString(addressBytes[:len(addressBytes)-4]) // 마지막 4바이트(체크섬) 제거
+// 	return "0x" + hexAddress
+// }
